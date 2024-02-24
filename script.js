9ae6454655b0ab6b33772ca619bdfb1b951176f6
Yusuke0620 <boston19851229@gmail.com>
2024-02-24 11:42:54 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const fruits = ["りんご", "バナナ", "みかん"];
+console.log(fruits);
+
+// スイカを追加
+fruits.push("スイカ");
+console.log(fruits);
+
+// スイカを削除
+fruits.pop(4);
+console.log(fruits);
+
+
+const colors = ["赤", "青", "緑"];
+console.log(colors);
+colors.push("黄色");
+console.log(colors);
+
+colors.pop(4);
+console.log(colors);
+
+colors.push("紫");
+console.log(colors);
+
+colors.shift();
+console.log(colors);
+
+colors.pop();
+console.log(colors);
